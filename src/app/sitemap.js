298d6d5f52cc7e3929362f2c634ed70c0d1c2896
samuelhenig <samuelhenig@gmail.com>
2026-05-03 00:47:19 -0400

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.henigfinancial.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.henigfinancial.com/articles",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.henigfinancial.com/articles/start-financial-clarity",
+      lastModified: new Date(),
+    },
+  ];
+}
