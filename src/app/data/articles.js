5d6087f9450c20b77ebb11a62efc449e2ce8d52c
samuelhenig <samuelhenig@gmail.com>
2026-05-03 00:07:34 -0400

    Create articles.js

---
+++ src/app/data/articles.js
@@ -0,0 +1,10 @@
+export const articles = [
+  {
+    title: "How to Start Getting Financial Clarity Without Feeling Overwhelmed",
+    description:
+      "A simple first step for families who want more control but don’t know where to begin.",
+    readTime: "3 min read",
+    author: "Shmily Henig, Founder",
+    href: "/articles/start-financial-clarity",
+  },
+];
