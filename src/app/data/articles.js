bc05269fd9c4c23c725e1dcbde589c7a79b432bd
samuelhenig <samuelhenig@gmail.com>
2026-05-05 11:00:03 -0400

    Update articles.js

--- src/app/data/articles.js
+++ src/app/data/articles.js
@@ -1,9 +1,1 @@
-import { startFinancialClarity } from "../content/articles/start-financial-clarity";
-import { whyBudgetingDoesntWork } from "../content/articles/why-budgeting-doesnt-work";
-import { theDropIsPartOfTheJump } from "../content/articles/the-drop-is-part-of-the-jump";
-
-export const articles = [
-  startFinancialClarity,
-  whyBudgetingDoesntWork,
-  theDropIsPartOfTheJump,
-];
+export { articles } from "../content/articles";
