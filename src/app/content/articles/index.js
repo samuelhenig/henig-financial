c628f030a7603690f3ae3f2373bf4f627cf32563
samuelhenig <samuelhenig@gmail.com>
2026-05-05 10:59:43 -0400

    Create index.js

---
+++ src/app/content/articles/index.js
@@ -0,0 +1,9 @@
+import { startFinancialClarity } from "./start-financial-clarity";
+import { whyBudgetingDoesntWork } from "./why-budgeting-doesnt-work";
+import { theDropIsPartOfTheJump } from "./the-drop-is-part-of-the-jump";
+
+export const articles = [
+  startFinancialClarity,
+  whyBudgetingDoesntWork,
+  theDropIsPartOfTheJump,
+];
